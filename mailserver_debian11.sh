4309ede5a9f2d8cf4df6260612edd1b54d5c1040
Alfarizkin <115212890+Alfarizkin@users.noreply.github.com>
2024-07-26 05:58:13 +0700

    Update mailserver_debian11.sh

--- mailserver_debian11.sh
+++ mailserver_debian11.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Fungsi untuk instalasi Postfix, Dovecot, dan Apache2
-
 install_mail_server() {
     sudo apt update
     sudo apt install -y postfix dovecot-imapd dovecot-pop3d apache2
@@ -31,6 +30,7 @@ install_mariadb() {
     echo -n "Input Password untuk user database: "
     read pass
     echo
+
     # Create a new MariaDB user and grant privileges
     sudo mysql -u root <<EOF
 CREATE USER IF NOT EXISTS 'roundcube'@'localhost' IDENTIFIED BY '$pass';
@@ -44,27 +44,34 @@ EOF
 install_roundcube() {
     local server_ip=$1
     local dns=$2
+
     sudo apt-get install -y roundcube
-    
-    # Backup original config.inc.php
-    sudo cp /etc/roundcube/config.inc.php /etc/roundcube/config.inc.php.bak
+
+    # Backup original config.inc.php jika belum ada
+    if [ ! -f /etc/roundcube/config.inc.php.bak ]; then
+        sudo cp /etc/roundcube/config.inc.php /etc/roundcube/config.inc.php.bak
+    fi
 
     # Update config.inc.php with the required configurations
-    echo "\$config['smtp_server'] = '$dns';" | sudo tee -a /etc/roundcube/config.inc.php
     echo "\$config['imap_host'] = '$dns:143';" | sudo tee -a /etc/roundcube/config.inc.php
+    sudo sed -i "s|\(\$config\['smtp_server'\] = \).*;|\1'$dns';|" /etc/roundcube/config.inc.php
     sudo sed -i "s|\(\$config\['smtp_port'\] = \).*|\1'25';|" /etc/roundcube/config.inc.php
     sudo sed -i "s|\(\$config\['smtp_user'\] = \).*|\1'';|" /etc/roundcube/config.inc.php
     sudo sed -i "s|\(\$config\['smtp_pass'\] = \).*|\1'';|" /etc/roundcube/config.inc.php
 
-    # Backup original apache.conf
-    sudo cp /etc/roundcube/apache.conf /etc/roundcube/apache.conf.bak
-    
+    # Backup original apache.conf jika belum ada
+    if [ ! -f /etc/roundcube/apache.conf.bak ]; then
+        sudo cp /etc/roundcube/apache.conf /etc/roundcube/apache.conf.bak
+    fi
+
     # Update apache.conf with the required configurations
     sudo sed -i "s|#\s*Alias /roundcube /var/lib/roundcube/|Alias /roundcube /var/lib/roundcube/|" /etc/roundcube/apache.conf
     sudo sed -i "s|<Directory /var/lib/roundcube/public_html/>|<Directory /var/lib/roundcube/>|" /etc/roundcube/apache.conf
 
-    # Backup original 000-default.conf
-    sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.bak
+    # Backup original 000-default.conf jika belum ada
+    if [ ! -f /etc/apache2/sites-available/000-default.conf.bak ]; then
+        sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.bak
+    fi
 
     # Update 000-default.conf with the required configurations
     sudo sed -i "s|<VirtualHost \*:80>|<VirtualHost $server_ip:80>|" /etc/apache2/sites-available/000-default.conf
@@ -73,6 +80,7 @@ install_roundcube() {
 
     # Restart Apache to apply changes
     sudo systemctl restart apache2
+
     echo "Roundcube installation and configuration completed."
 }
 
@@ -88,7 +96,7 @@ install_bind9() {
     # Tambahkan zona untuk domain jika belum ada
     if ! grep -q "zone \"$dns\"" /etc/bind/named.conf.local; then
         cat <<EOF | sudo tee -a /etc/bind/named.conf.local
-    zone "$dns" {
+zone "$dns" {
     type master;
     file "/etc/bind/db.$dns";
 };
@@ -100,127 +108,65 @@ EOF
 ; BIND data file for $dns
 ;
 \$TTL    604800
-
 @       IN      SOA     ns1.$dns. admin.$dns. (
-
                               2         ; Serial
-
                          604800         ; Refresh
-
                           86400         ; Retry
-
                         2419200         ; Expire
-
                          604800 )       ; Negative Cache TTL
-
 ;
-
 @       IN      NS      ns1.$dns.
-
 ns1     IN      A       $server_ip
-
 @       IN      A       $server_ip
-
 EOF
-
     else
-
         # Update existing zone file with new IP
-
         sudo sed -i "s|IN\s*A\s*[0-9.]*|IN A $server_ip|" /etc/bind/db.$dns
-
     fi
 
-
-
     # Menambahkan konfigurasi nama server di resolv.conf
-
     echo "nameserver $server_ip" | sudo tee /etc/resolvconf/resolv.conf.d/head
 
-
-
     # Restart resolvconf untuk menerapkan perubahan
-
     sudo systemctl restart resolvconf
-
     sudo resolvconf -u
 
-
-
     # Restart BIND9 untuk menerapkan perubahan
-
     sudo systemctl restart bind9
 
-
-
     echo "BIND9 installation and configuration completed."
-
 }
-
-
 
 # Proses utama
-
 main() {
-
     echo -n "Masukan IP Server anda (IP Linux Anda): "
-
     read server_ip
 
-
-
     echo -n "Masukan Domain Server, contoh (smofi.com): "
-
     read dns
 
-
-
     # Install Postfix, Dovecot, Thunderbird, and Apache2
-
     install_mail_server
 
-
-
     # Baca IP baru dari input pengguna
-
     echo -n "Masukan IP Network Linux Anda contoh (192.168.100.0/24): "
-
     read mynetworks_ip
 
-
-
     # Tambahkan IP ke konfigurasi mynetworks
-
     add_ip_to_mynetworks $mynetworks_ip
 
-
-
     # Restart Postfix untuk menerapkan perubahan
-
     sudo systemctl restart postfix
 
-
-
     # Install dan konfigurasi MariaDB
-
     install_mariadb
 
-
-
     # Install dan konfigurasi Roundcube
-
     install_roundcube $server_ip $dns
 
-
-
     # Install dan konfigurasi BIND9
-
     install_bind9 $server_ip $dns
-
 }
 
-
-
 # Jalankan proses utama
-
 main
